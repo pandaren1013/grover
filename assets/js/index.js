34c00012015c2dce8437a4debedb252fee86d2e7
pandaren1013 <pandaren1013@outlook.com>
2023-08-07 22:33:47 +0900

    initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+function openNavar(){
+    document.getElementById("sideNavigationBar").style.width="35%";
+    
+    //document.getElementById("openNavar").style.display="none";
+}
+function closeNavbar(){
+    document.getElementById("sideNavigationBar").style.width="0%";
+    // document.getElementById("openNavar").style.display="block";
+}
